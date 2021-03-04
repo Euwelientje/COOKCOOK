751085ea563cf05a4934b3540e2c51dac85493f7
Euwelientje <77613393+Euwelientje@users.noreply.github.com>
2021-03-04 21:37:15 +0100

    Add files via upload

---
+++ cookcookWebsite/Script/app.js
@@ -0,0 +1,14 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navLinks');
+
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("navActive");
+    });
+}
+
+navSlide();
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
